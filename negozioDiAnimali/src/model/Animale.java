d0cd0161fd3815be4acb5f8d8e909e0497159a4e
ANT-2307 <saccoantonio.1@tiscali.it>
2025-01-31 16:18:11 +0100

    Add files via upload

---
+++ negozioDiAnimali/src/model/Animale.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.sql.Date;
+
+public class Animale {
+	public int chip;
+	public String razza;
+	public String taglia;
+	public Date dataDiNascita;
+	public String sesso;
+}
